6638f5d86e59f2bfc8c4a6ddb9f4322bbccbe46f
Alfred Zhong <alfred.zhong@centre.edu>
2019-10-29 17:27:37 -0400

    a

--- project.js
+++ project.js
@@ -18,20 +18,20 @@ var seth1 = function()
     }
 
 var filmsname= function (film)
-{
-d3.select(".films")
+{ 
+    d3.select(".films")
     .selectAll("th")
     .data(film)
     .enter()
     .append("th")
     .text(function(d){return d.title;})
     .on("click",function(f){intro(f)})
-    {removeintro();
-    intro(film,films)}
+    
 }
 
 var removeintro=function()
-{d3.selectALL(".intro*").remove()}
+{d3.selectAll(".intro").remove();}
+
 
 var intro=function (film)
 {
